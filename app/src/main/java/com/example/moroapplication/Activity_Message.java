5c78916eb2cb93ef5247b5e50efede0efebbfe77
Phuoctoan091102 <toanlp20411@st.uel.edu.vn>
2022-11-30 23:33:35 +0700

    Message_Index

---
+++ app/src/main/java/com/example/moroapplication/Activity_Message.java
@@ -0,0 +1,13 @@
+package com.example.moroapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Message extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+    }
+}
