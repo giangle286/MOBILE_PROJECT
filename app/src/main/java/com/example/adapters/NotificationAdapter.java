632eb3235c354ac40115f233b1f6cd422f10c723
Phuoctoan091102 <toanlp20411@st.uel.edu.vn>
2022-11-30 21:55:30 +0700

    Notification_Complete

---
+++ app/src/main/java/com/example/adapters/NotificationAdapter.java
@@ -0,0 +1,64 @@
+package com.example.adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.models.Notification;
+import com.example.moroapplication.R;
+
+import java.util.ArrayList;
+
+public class NotificationAdapter extends BaseAdapter {
+    private Activity context;
+    ArrayList<Notification> notifications;
+
+    public NotificationAdapter(Activity context, ArrayList<Notification> notifications) {
+        this.context = context;
+        this.notifications = notifications;
+    }
+
+    @Override
+    public int getCount() {
+        return notifications.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return notifications.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if(view == null){
+            //Link item view
+            holder = new NotificationAdapter.ViewHolder();
+            LayoutInflater inflater = context.getLayoutInflater();
+            view = inflater.inflate(R.layout.itemlist_notification, null);
+            holder.txtNotification = view.findViewById(R.id.txtNotification);
+            view.setTag(holder);
+        }else {
+            holder = (NotificationAdapter.ViewHolder) view.getTag();
+
+        }
+        holder.txtNotification.setText(notifications.get(i).getNotification());
+        return view;
+    }
+
+    public static class ViewHolder {
+        TextView txtNotification;
+
+
+    }
+}
