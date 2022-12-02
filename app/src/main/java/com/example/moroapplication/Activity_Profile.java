a5c1ec3dc8b053851a28a6c1ab68e978dcd6221c
Phuoctoan091102 <toanlp20411@st.uel.edu.vn>
2022-12-02 11:02:23 +0700

    Thanh Dieu Huong_Complete

--- app/src/main/java/com/example/moroapplication/Activity_Profile.java
+++ app/src/main/java/com/example/moroapplication/Activity_Profile.java
@@ -1,8 +1,13 @@
 package com.example.moroapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Activity_Profile extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class Activity_Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        bottomNav();
+    }
+
+    private void bottomNav() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_notification);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_account:
+                        return true;
+                    case R.id.action_homepage:
+                        startActivity(new Intent(getApplicationContext(),HomePage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.action_news:
+                        startActivity(new Intent(getApplicationContext(),Activity_Blog.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.action_decor:
+                        startActivity(new Intent(getApplicationContext(),Activity_Deco.class));
+                        overridePendingTransition(0,0);
+                    case R.id.action_notification:
+                        startActivity(new Intent(getApplicationContext(),Activity_Notification.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+
+                return false;
+            }
+        });
     }
 }
