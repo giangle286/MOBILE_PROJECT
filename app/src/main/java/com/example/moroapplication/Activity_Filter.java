5a9127bc257c2847e84062b7c11cf5098d0e03d4
minhoa2811 <113772462+minhoa2811@users.noreply.github.com>
2022-12-02 23:40:07 +0700

    thanh filter1

---
+++ app/src/main/java/com/example/moroapplication/Activity_Filter.java
@@ -0,0 +1,14 @@
+package com.example.moroapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_Filter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
