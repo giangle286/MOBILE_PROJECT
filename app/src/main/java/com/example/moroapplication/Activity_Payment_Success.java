479e50411c229777c04df0a74157bae763c473c4
Giang Le <gianglt20411@st.uel.edu.vn>
2022-12-03 01:38:03 +0700

    create_payment success

---
+++ app/src/main/java/com/example/moroapplication/Activity_Payment_Success.java
@@ -0,0 +1,14 @@
+package com.example.moroapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Payment_Success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_success);
+    }
+}
