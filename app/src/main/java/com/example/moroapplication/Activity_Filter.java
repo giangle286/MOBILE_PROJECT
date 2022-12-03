d1739cb1839d7e053c8b33d18635967fc3da9a70
minhoa2811 <113772462+minhoa2811@users.noreply.github.com>
2022-12-03 09:01:15 +0700

    thanh filter1

--- app/src/main/java/com/example/moroapplication/Activity_Filter.java
+++ app/src/main/java/com/example/moroapplication/Activity_Filter.java
@@ -1,14 +1,35 @@
 package com.example.moroapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_Filter extends AppCompatActivity {
+    String[] items =  {"Hồ Chí Minh","Bình Dương","Hà Nội","Hải Phòng"};
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter<String> adapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        autoCompleteTxt=findViewById(R.id.auto_complete_txt);
+
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_filter1,items);
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
