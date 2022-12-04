0d20d0111da68ac180e3b73eb0a4b64d14f30504
tuuy3n <uyenttt20411@st.uel.edu.vn>
2022-12-05 00:07:35 +0700

    Comment done

--- app/src/main/java/com/example/moroapplication/Activity_Deco_Details.java
+++ app/src/main/java/com/example/moroapplication/Activity_Deco_Details.java
@@ -5,11 +5,16 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.adapters.DecoCommentAdapter;
 import com.example.adapters.DecoSamplesAdapter;
 import com.example.models.Deco;
+import com.example.models.DecoComment;
 import com.example.models.DecoSamples;
 import com.example.moroapplication.databinding.ActivityDecoDetailsBinding;
 
@@ -22,6 +27,12 @@ public class Activity_Deco_Details extends AppCompatActivity {
     private ViewPager mViewPager;
     private CircleIndicator mCircleIndicator;
     private List<DecoSamples>mListDecoSamples;
+
+    private EditText edtCmt;
+    private ImageView imvSendCmt;
+    ListView lvCmt;
+    DecoCommentAdapter decoCommentAdapter;
+    ArrayList<DecoComment> decoComment;
 
     private Handler mHandler=new Handler();
     private Runnable mRunnable=new Runnable() {
@@ -42,6 +53,11 @@ public class Activity_Deco_Details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deco_details);
+        linkView();
+        addEvent();
+        initAdapter();
+        initData();
+
         mViewPager=findViewById(R.id.deco_viewpager);
         mCircleIndicator=findViewById(R.id.deco_circle_indicator);
         mListDecoSamples=getListDecoSamples();
@@ -92,5 +108,35 @@ public class Activity_Deco_Details extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         mHandler.postDelayed(mRunnable,3000);
+    }
+
+    private void linkView() {
+        edtCmt=findViewById(R.id.edt_Cmt);
+        imvSendCmt=findViewById(R.id.imv_SendCmt);
+        lvCmt=findViewById(R.id.lvCmt);
+    }
+
+    private void initData() {
+        decoComment=new ArrayList<>();
+        decoComment.add(new DecoComment("Rất xinh đẹp tuyệt vời","Linh Anh"));
+        decoComment.add(new DecoComment("Xịn quá ạ","Khánh Ngân"));
+        decoCommentAdapter=new DecoCommentAdapter(Activity_Deco_Details.this,R.layout.item_cmt,decoComment);
+        lvCmt.setAdapter(decoCommentAdapter);
+    }
+
+    private void addEvent() {
+        imvSendCmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                comment=new ArrayList<>();
+                decoComment.add(new DecoComment(edtCmt.getText().toString(),"Tiểu Vy"));
+                decoCommentAdapter=new DecoCommentAdapter(Activity_Deco_Details.this,R.layout.item_cmt,decoComment);
+                lvCmt.setAdapter(decoCommentAdapter);
+            }
+        });
+    }
+
+    private void initAdapter() {
+
     }
 }
