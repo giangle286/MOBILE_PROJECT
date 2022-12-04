e2c7e1c1d8f2314d9d0ab563c26154b1696931c4
truclamm <lamvnt20411@st.uel.edu.vn>
2022-12-04 22:19:08 +0700

    Profile_logout_update

--- app/src/main/java/com/example/moroapplication/Activity_Profile_Detail2_Update_Info.java
+++ app/src/main/java/com/example/moroapplication/Activity_Profile_Detail2_Update_Info.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_Profile_Detail2_Update extends AppCompatActivity {
+public class Activity_Profile_Detail2_Update_Info extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_detail2_update);
+        setContentView(R.layout.activity_profile_detail2_update_info);
     }
 
 }
