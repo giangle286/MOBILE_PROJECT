223b8e2827acb58430c4b0f9ef858be98a3c6752
truclamm <lamvnt20411@st.uel.edu.vn>
2022-12-05 11:55:49 +0700

    Profile_update

---
+++ app/src/main/java/com/example/models/ProfileChoosing.java
@@ -0,0 +1,19 @@
+package com.example.models;
+
+public class ProfileChoosing {
+        String profileTitle;
+
+
+    public String getProfileTitle() {
+        return profileTitle;
+    }
+
+    public void setProfileTitle(String profileTitle) {
+        this.profileTitle = profileTitle;
+    }
+
+    public ProfileChoosing( String profileTitle) {
+        this.profileTitle = profileTitle;
+    }
+
+}
